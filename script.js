1bef82668dc04643b1c744ca03345e6367266cd7
UNEMPLOYED CODER <123456789+odin@users.noreply.github.com>
2023-11-02 13:51:44 +1100

    all

---
+++ script.js
@@ -0,0 +1,9 @@
+const gridContainer = document.getElementById('gridContainer');
+// Create 16x16 grid
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridContainer.appendChild(gridItem);
+    }
+}
